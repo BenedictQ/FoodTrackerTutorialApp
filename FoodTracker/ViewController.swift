261370a66d59e7479fae2405eaee85efdabb818b
Benedict Quinn <bquinn@benedicts-mac-mini.local>
2018-09-26 15:45:07 +0100

    Initial Commit

---
+++ FoodTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Benedict Quinn on 26/09/2018.
+//  Copyright © 2018 Benedict Quinn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
